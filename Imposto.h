94f8909ff300c7903ce7ea58e7eb80d17dfced63
DOUGLAS FAGUNDES KLEIN <12205173@pucrs.br>
2019-09-26 18:55:13 -0300

    classe inicial

--- Imposto.h
+++ Imposto.h
@@ -8,9 +8,24 @@ class Imposto
         Imposto();
         virtual ~Imposto();
 
+        string nome;
+        int cpf;
+        int idade;
+        int dep;
+        int declaracao;
+        void Entrada();
+
     protected:
 
     private:
+
+        double base;
+        double basef;
+        double renda;
+        double contrib;
+        double imposto;
+        double DeclarSimp();
+        double DeclarComp();
 };
 
 #endif // IMPOSTO_H
